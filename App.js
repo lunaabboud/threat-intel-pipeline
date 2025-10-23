773a744412abd38b66ca5b882fe504bd834eae7a
lunaabboud <160365172+lunaabboud@users.noreply.github.com>
2025-10-23 09:40:36 +0100

    Update and rename App.js to src/App.js

--- App.js
+++
@@ -1,7 +0,0 @@
-import ThreatIntelPipeline from './ThreatIntelPipeline';
-
-function App() {
-  return <ThreatIntelPipeline />;
-}
-
-export default App;
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ThreatIntelPipeline from './components/ThreatIntelPipeline'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <ThreatIntelPipeline />
+    </div>
+  )
+}
+
+export default App
